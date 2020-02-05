e11ad2ebbebf0886f53354c66f6357fec0affe14
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-02-05 13:29:45 +0300

    feat: add implementation for Filters. Advanced. To be continue... task.

---
+++ FilterExtensionMethod/ArrayExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FilterTemplateMethod;
+
+namespace FilterExtensionMethod
+{
+    /// <summary>Provides extension method to filter array by specified sign.</summary>
+    public static class ArrayExtension
+    {
+        /// <summary>Filters array by specified sign.</summary>
+        /// <param name="array">Array.</param>
+        /// <param name="filter">Specified filter.</param>
+        /// <returns>Returns new filtered array.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <em>filter</em> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
+        public static int[] FilterBy(this int[] array, Filter filter)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be empty.");
+            }
+
+            return filter.FilterArrayByKey(array);
+        }
+    }
+}
